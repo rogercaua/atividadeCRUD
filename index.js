5f5d139bf14e9a0ca5bbfea940c6c70bd045e365
rogercaua <rogercaua@outlook.com.br>
2024-08-30 22:29:21 -0300

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+
+const cursos = ["Node JS", "JavaScript", "React Native"];
+
+server.get("/cursos", (req,res) => {
+   return res.json(cursos);
+})
+
+server.get("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
+})
+
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+  
+  return res.json(cursos);
+})
+
+server.put("cursos/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+})
+
+server.delete("cursos/:index", (req, res) => {
+  const {index} = req.params;
+  
+  cursos.splice(index,1)
+  return res.json({message: "Curso deletado com sucesso !"});
+})
+
+server.listen(3000)
